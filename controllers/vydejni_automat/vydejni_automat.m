82815128b6126722f34fcf3c821636988c28424e
Horac09 <74052852+Horac09@users.noreply.github.com>
2020-12-11 13:21:00 +0100

    added next 4 pistons, shelfs and shelf blocks

--- controllers/vydejni_automat/vydejni_automat.m
+++ controllers/vydejni_automat/vydejni_automat.m
@@ -22,7 +22,7 @@ wb_motor_set_velocity(motor, 0.025);
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
-i = 0
+
 for i = 0:0.01:0.03
 %while wb_robot_step(TIME_STEP) ~= -1
 
@@ -51,7 +51,118 @@ wb_motor_set_velocity(motor, 0.025);
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
-i = 0
+
+for i = 0:0.01:0.03
+%while wb_robot_step(TIME_STEP) ~= -1
+
+  % read the sensors, e.g.:
+  %  rgb = wb_camera_get_image(camera);
+
+  % Process here sensor data, images, etc.
+
+  % send actuator commands, e.g.:
+  %  wb_motor_set_postion(motor, 10.0);
+
+  wb_motor_set_position(motor, i);
+
+  % if your code plots some graphics, it needs to flushed like this:
+  drawnow;
+
+end
+
+motor = wb_robot_get_device('piston 3');
+wb_motor_set_velocity(motor, 0.025);
+%  wb_camera_enable(camera, TIME_STEP);
+%  motor = wb_robot_get_device('motor');
+
+% main loop:
+% perform simulation steps of TIME_STEP milliseconds
+% and leave the loop when Webots signals the termination
+%
+
+for i = 0:0.01:0.03
+%while wb_robot_step(TIME_STEP) ~= -1
+
+  % read the sensors, e.g.:
+  %  rgb = wb_camera_get_image(camera);
+
+  % Process here sensor data, images, etc.
+
+  % send actuator commands, e.g.:
+  %  wb_motor_set_postion(motor, 10.0);
+
+  wb_motor_set_position(motor, i);
+
+  % if your code plots some graphics, it needs to flushed like this:
+  drawnow;
+
+end
+
+motor = wb_robot_get_device('piston 4');
+wb_motor_set_velocity(motor, 0.025);
+%  wb_camera_enable(camera, TIME_STEP);
+%  motor = wb_robot_get_device('motor');
+
+% main loop:
+% perform simulation steps of TIME_STEP milliseconds
+% and leave the loop when Webots signals the termination
+%
+for i = 0:0.01:0.03
+%while wb_robot_step(TIME_STEP) ~= -1
+
+  % read the sensors, e.g.:
+  %  rgb = wb_camera_get_image(camera);
+
+  % Process here sensor data, images, etc.
+
+  % send actuator commands, e.g.:
+  %  wb_motor_set_postion(motor, 10.0);
+
+  wb_motor_set_position(motor, i);
+
+  % if your code plots some graphics, it needs to flushed like this:
+  drawnow;
+
+end
+
+motor = wb_robot_get_device('piston 5');
+wb_motor_set_velocity(motor, 0.025);
+%  wb_camera_enable(camera, TIME_STEP);
+%  motor = wb_robot_get_device('motor');
+
+% main loop:
+% perform simulation steps of TIME_STEP milliseconds
+% and leave the loop when Webots signals the termination
+%
+
+for i = 0:0.01:0.03
+%while wb_robot_step(TIME_STEP) ~= -1
+
+  % read the sensors, e.g.:
+  %  rgb = wb_camera_get_image(camera);
+
+  % Process here sensor data, images, etc.
+
+  % send actuator commands, e.g.:
+  %  wb_motor_set_postion(motor, 10.0);
+
+  wb_motor_set_position(motor, i);
+
+  % if your code plots some graphics, it needs to flushed like this:
+  drawnow;
+
+end
+
+motor = wb_robot_get_device('piston 6');
+wb_motor_set_velocity(motor, 0.025);
+%  wb_camera_enable(camera, TIME_STEP);
+%  motor = wb_robot_get_device('motor');
+
+% main loop:
+% perform simulation steps of TIME_STEP milliseconds
+% and leave the loop when Webots signals the termination
+%
+
 for i = 0:0.01:0.03
 %while wb_robot_step(TIME_STEP) ~= -1
 
