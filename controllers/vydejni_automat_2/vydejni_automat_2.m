dafdcb540aa2b1a6f66cbcbbdbd4e20193d39d0a
Horac09 <74052852+Horac09@users.noreply.github.com>
2020-12-16 19:54:49 +0100

    Added controller changes

---
+++ controllers/vydejni_automat_2/vydejni_automat_2.m
@@ -0,0 +1,82 @@
+% MATLAB controller for Webots
+% File:          vydejni_automat_2.m
+% Date:
+% Description:
+% Author:
+% Modifications:
+
+% uncomment the next two lines if you want to use
+% MATLAB's desktop to interact with the controller:
+%desktop;
+%keyboard;
+
+TIME_STEP = 64;
+
+motor_1 = wb_robot_get_device('piston 1');
+wb_motor_set_velocity(motor_1, 0.025);
+pos_1 = 0;
+
+motor_2 = wb_robot_get_device('piston 2');
+wb_motor_set_velocity(motor_2, 0.025);
+pos_2 = 0;
+
+motor_3 = wb_robot_get_device('piston 3');
+wb_motor_set_velocity(motor_3, 0.025);
+pos_3 = 0;
+
+motor_4 = wb_robot_get_device('piston 4');
+wb_motor_set_velocity(motor_4, 0.025);
+pos_4 = 0;
+
+motor_5 = wb_robot_get_device('piston 5');
+wb_motor_set_velocity(motor_5, 0.025);
+pos_5 = 0;
+
+motor_6 = wb_robot_get_device('piston 6');
+wb_motor_set_velocity(motor_6, 0.025);
+pos_6 = 0;
+
+f = true;
+
+while wb_robot_step(TIME_STEP) ~= -1
+  while f
+    x = input('Zadejte cislo 1-6  Vase cislo: ')
+    if x >= 1 && x <= 6
+    f = false;
+end
+end
+
+switch x
+
+case 1
+  pos_1 = pos_1 + 0.03;
+  wb_motor_set_position(motor_1, pos_1);
+  x = -1;
+  
+case 2
+  pos_2 = pos_2 + 0.03;
+  wb_motor_set_position(motor_2, pos_2);
+  x = -1;
+
+case 3
+  pos_3 = pos_3 + 0.03;
+  wb_motor_set_position(motor_3, pos_3);
+  x = -1;
+
+case 4
+  pos_4 = pos_4 + 0.03;
+  wb_motor_set_position(motor_4, pos_4);
+  x = -1;
+
+case 5
+  pos_5 = pos_5 + 0.03;
+  wb_motor_set_position(motor_5, pos_5);
+  x = -1;
+
+case 6
+  pos_6 = pos_6 + 0.03;
+  wb_motor_set_position(motor_6, pos_6);
+  x = -1;
+  f = true;
+end
+end
