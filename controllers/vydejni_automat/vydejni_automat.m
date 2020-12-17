b70a55960e31d5ecd3e32c65c6368bba1dcafa32
Horac09 <74052852+Horac09@users.noreply.github.com>
2020-12-17 15:12:56 +0100

    small changes

--- controllers/vydejni_automat/vydejni_automat.m
+++ controllers/vydejni_automat/vydejni_automat.m
@@ -10,174 +10,80 @@
 %desktop;
 %keyboard;
 
-TIME_STEP = 64;
+TIME_STEP = 1024;
 
+motor_1 = wb_robot_get_device('piston 1');
+wb_motor_set_velocity(motor_1, 0.025);
+pos_1 = 0;
 
-% get and enable devices, e.g.:
-motor = wb_robot_get_device('piston 1');
-wb_motor_set_velocity(motor, 0.025);
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
+motor_2 = wb_robot_get_device('piston 2');
+wb_motor_set_velocity(motor_2, 0.025);
+pos_2 = 0;
 
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
+motor_3 = wb_robot_get_device('piston 3');
+wb_motor_set_velocity(motor_3, 0.025);
+pos_3 = 0;
 
-for i = 0:0.01:0.03
-%while wb_robot_step(TIME_STEP) ~= -1
+motor_4 = wb_robot_get_device('piston 4');
+wb_motor_set_velocity(motor_4, 0.025);
+pos_4 = 0;
 
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
+motor_5 = wb_robot_get_device('piston 5');
+wb_motor_set_velocity(motor_5, 0.025);
+pos_5 = 0;
 
-  % Process here sensor data, images, etc.
+motor_6 = wb_robot_get_device('piston 6');
+wb_motor_set_velocity(motor_6, 0.025);
+pos_6 = 0;
 
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
+f = true;
 
-  wb_motor_set_position(motor, i);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
-end
-
-% cleanup code goes here: write data to files, etc.
-motor = wb_robot_get_device('piston 2');
-wb_motor_set_velocity(motor, 0.025);
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
-
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
-
-for i = 0:0.01:0.03
-%while wb_robot_step(TIME_STEP) ~= -1
-
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
-
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  wb_motor_set_position(motor, i);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
-end
-
-motor = wb_robot_get_device('piston 3');
-wb_motor_set_velocity(motor, 0.025);
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
-
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
-
-for i = 0:0.01:0.03
-%while wb_robot_step(TIME_STEP) ~= -1
-
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
-
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  wb_motor_set_position(motor, i);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
-end
-
-motor = wb_robot_get_device('piston 4');
-wb_motor_set_velocity(motor, 0.025);
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
-
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
-for i = 0:0.01:0.03
-%while wb_robot_step(TIME_STEP) ~= -1
-
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
-
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  wb_motor_set_position(motor, i);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
-end
-
-motor = wb_robot_get_device('piston 5');
-wb_motor_set_velocity(motor, 0.025);
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
-
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
-
-for i = 0:0.01:0.03
-%while wb_robot_step(TIME_STEP) ~= -1
-
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
-
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  wb_motor_set_position(motor, i);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
-end
-
-motor = wb_robot_get_device('piston 6');
-wb_motor_set_velocity(motor, 0.025);
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
-
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
-
-for i = 0:0.01:0.03
-%while wb_robot_step(TIME_STEP) ~= -1
-
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
-
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  wb_motor_set_position(motor, i);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
+while wb_robot_step(TIME_STEP) ~= -1
+    while f
+        x = input('Zadejte číslo 1-6  Vaše číslo: ')
+        if x >= 1 && x <= 6
+            f = false;
+        else disp('Neplatné číslo!')
+        end
+    end
+    
+    switch x
+        
+        case 1
+            disp('Produkt č.1 je vydáván')
+            pos_1 = pos_1 + 0.03;
+            wb_motor_set_position(motor_1, pos_1);
+            x = -1;
+            
+        case 2
+            disp('Produkt č.2 je vydáván')
+            pos_2 = pos_2 + 0.03;
+            wb_motor_set_position(motor_2, pos_2);
+            x = -1;
+            
+        case 3
+            disp('Produkt č.3 je vydáván')
+            pos_3 = pos_3 + 0.03;
+            wb_motor_set_position(motor_3, pos_3);
+            x = -1;
+            
+        case 4
+            disp('Produkt č.4 je vydáván')
+            pos_4 = pos_4 + 0.03;
+            wb_motor_set_position(motor_4, pos_4);
+            x = -1;
+            
+        case 5
+            disp('Produkt č.5 je vydáván')
+            pos_5 = pos_5 + 0.03;
+            wb_motor_set_position(motor_5, pos_5);
+            x = -1;
+            
+        case 6
+            disp('Produkt č.6 je vydáván')
+            pos_6 = pos_6 + 0.03;
+            wb_motor_set_position(motor_6, pos_6);
+            x = -1;
+            
+    end
 end
