1388b9f00ea1f842217959abdf0c5e98e7c8ad9c
Horac09 <74052852+Horac09@users.noreply.github.com>
2020-12-17 17:34:43 +0100

    added physics and next changes with textures

--- controllers/vydejni_automat/vydejni_automat.m
+++ controllers/vydejni_automat/vydejni_automat.m
@@ -10,80 +10,104 @@
 %desktop;
 %keyboard;
 
-TIME_STEP = 1024;
+TIME_STEP = 3000;
 
 motor_1 = wb_robot_get_device('piston 1');
-wb_motor_set_velocity(motor_1, 0.025);
+wb_motor_set_velocity(motor_1, 0.035);
 pos_1 = 0;
 
 motor_2 = wb_robot_get_device('piston 2');
-wb_motor_set_velocity(motor_2, 0.025);
+wb_motor_set_velocity(motor_2, 0.035);
 pos_2 = 0;
 
 motor_3 = wb_robot_get_device('piston 3');
-wb_motor_set_velocity(motor_3, 0.025);
+wb_motor_set_velocity(motor_3, 0.035);
 pos_3 = 0;
 
 motor_4 = wb_robot_get_device('piston 4');
-wb_motor_set_velocity(motor_4, 0.025);
+wb_motor_set_velocity(motor_4, 0.035);
 pos_4 = 0;
 
 motor_5 = wb_robot_get_device('piston 5');
-wb_motor_set_velocity(motor_5, 0.025);
+wb_motor_set_velocity(motor_5, 0.035);
 pos_5 = 0;
 
 motor_6 = wb_robot_get_device('piston 6');
-wb_motor_set_velocity(motor_6, 0.025);
+wb_motor_set_velocity(motor_6, 0.035);
 pos_6 = 0;
 
 f = true;
 
 while wb_robot_step(TIME_STEP) ~= -1
     while f
-        x = input('Zadejte číslo 1-6  Vaše číslo: ')
+        x = input('Vyberte produkt 1-6  Váš produkt: ')
         if x >= 1 && x <= 6
             f = false;
-        else disp('Neplatné číslo!')
+        else disp('Špatný výběr produktu!')
         end
     end
     
     switch x
         
         case 1
-            disp('Produkt č.1 je vydáván')
-            pos_1 = pos_1 + 0.03;
+            disp('Váš Grepl nápoj je vydáván')
+            pos_1 = pos_1 + 0.045;
             wb_motor_set_position(motor_1, pos_1);
+            if pos_1 >= 0.09
+              pos_1 = -0.045;
+            end
             x = -1;
+            f = true;
             
         case 2
-            disp('Produkt č.2 je vydáván')
-            pos_2 = pos_2 + 0.03;
+            disp('Vaše pepsi je vydávána')
+            pos_2 = pos_2 + 0.045;
             wb_motor_set_position(motor_2, pos_2);
+            if pos_2 >= 0.09
+              pos_2 = -0.045;
+            end
             x = -1;
+            f = true;
             
         case 3
-            disp('Produkt č.3 je vydáván')
-            pos_3 = pos_3 + 0.03;
+            disp('Váš Red Bull je vydáván')
+            pos_3 = pos_3 + 0.045;
             wb_motor_set_position(motor_3, pos_3);
+            if pos_3 >= 0.09
+              pos_3 = -0.045;
+            end
             x = -1;
+            f = true;
             
         case 4
-            disp('Produkt č.4 je vydáván')
-            pos_4 = pos_4 + 0.03;
+            disp('Váš Sprite je vydáván')
+            pos_4 = pos_4 + 0.045;
             wb_motor_set_position(motor_4, pos_4);
+            if pos_4 >= 0.09
+              pos_4 = -0.045;
+            end
             x = -1;
+            f = true;
             
         case 5
-            disp('Produkt č.5 je vydáván')
-            pos_5 = pos_5 + 0.03;
+            disp('Vaše Coca Cola je vydávána')
+            pos_5 = pos_5 + 0.045;
             wb_motor_set_position(motor_5, pos_5);
+            if pos_5 >= 0.09
+              pos_5 = -0.045;
+            end
             x = -1;
+            f = true;
             
         case 6
-            disp('Produkt č.6 je vydáván')
-            pos_6 = pos_6 + 0.03;
+            disp('Vaše Fanta je vydávána')
+            pos_6 = pos_6 + 0.045;
             wb_motor_set_position(motor_6, pos_6);
+            if pos_6 >= 0.09
+              pos_6 = -0.045;
+            end
             x = -1;
+            f = true;
             
     end
 end
